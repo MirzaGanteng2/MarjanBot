415b2a6fa851f48f931cd1faadbd3c9ac46e2afc
MARJAN <97868744+MirzaGanteng2@users.noreply.github.com>
2022-01-18 18:20:46 +0800

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,7 @@
+Apt update && apt upgrade
+Apt install ffmpeg -y
+Apt install nodejs -y
+Apt install libwebp -y
+Apt install imagemagick -y 
+Git clone https://github.com/MirzaGanteng2/MarjanBot
+Npm i
